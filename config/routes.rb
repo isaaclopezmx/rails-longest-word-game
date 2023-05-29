3716fc4d55b94e42225095d7de47fd776cd40602
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-05-29 14:53:41 -0600

    score submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Defines the root path route ("/")
+    # root "articles#index"
+    root 'pages#run'
+    get "run", to: "pages#run"
+    get "score", to: "pages#score"
+  end
